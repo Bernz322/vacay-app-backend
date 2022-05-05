8a12b9d91f96ef2156ed1305d2ed423cb12b5ba8
Jeffrey Bernadas <bernadas_jeffrey@yahoo.com>
2022-05-05 13:52:56 +0800

    bug fixing

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -150,6 +150,7 @@ const getAllRooms = async (req, res) => {
 
         if (allRooms) {
             const toReturn = allRooms.map(room => {
+                const images = JSON.parse(room?.room_image)
                 const temp = {
                     id: room.id,
                     room_name: room.room_name,
@@ -167,7 +168,7 @@ const getAllRooms = async (req, res) => {
                     has_heating: room.has_heating,
                     has_internet: room.has_internet,
                     price: room.price,
-                    room_image: JSON.parse(room.room_image), // room_image returns a string so we have to convert it again.
+                    room_image: images, // room_image returns a string so we have to convert it again.
                     latitude: room.latitude,
                     longitude: room.longitude,
                     listing_status: room.listing_status,
