095fe0e5da1dfeac91c7ecbedb57a1dc794b562a
Jeffrey Bernadas <bernadas_jeffrey@yahoo.com>
2022-05-17 20:13:26 +0800

    remove token requirements when seeing room reviews

--- routes/reservationRoutes.js
+++ routes/reservationRoutes.js
@@ -6,7 +6,7 @@ router.post("/", validateToken, createReservation); // Create a reservation usin
 router.post("/check", checkAvailability); // Check room availability
 router.get("/reservations", validateToken, getAllUserReservation);   // Get all reservations created by the current user id
 router.get("/:id", validateToken, getReservationById);    // Get an specific reservation created by the current user and that room reserved
-router.get("/room/:id", validateToken, getRoomReservation);    // Get all reservations on a single room
+router.get("/room/:id", getRoomReservation);    // Get all reservations on a single room
 router.put("/", validateToken, updateReservation);    // Delete the reservation created by the current user by taking the reservation id
 router.delete("/:id", validateToken, deleteReservation);    // Delete the reservation created by the current user by taking the reservation id
 
